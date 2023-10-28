9c6a88bfadf5f815e259521490e4a9bde7913f39
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-10-29 00:22:02 +0700

    Added Investments model

---
+++ mulainvest/app/Models/Investments.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Investments extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'InvestmentID';
+
+    protected $fillable = [
+        'InvestmentName', 'InvestmentType', 'InvestmentDescription', 'Available', 'InvestmentPrice', 'MinimumOrder', 'MaximumOrder'
+    ];
+
+    // An investment has many assets
+    public function assets()
+    {
+        return $this->hasMany(Asset::class, 'InvestmentID');
+    }
+}
