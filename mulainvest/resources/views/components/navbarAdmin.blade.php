b7ee04f2f9180f103f754a4f05d51d662394d6d6
sfmuazam <sfmuazam@gmail.com>
2023-11-04 22:44:27 +0700

    Integrasi admin

--- mulainvest/resources/views/components/navbarAdmin.blade.php
+++ mulainvest/resources/views/components/navbarAdmin.blade.php
@@ -13,18 +13,18 @@
               <line x1="4" y1="18" x2="20" y2="18"></line>
             </svg>
           </button>
-          
-          
-        
+
+
+
           <div class="ms-0 ms-lg-4 me-4">
 
             <a href="" class="navbar-brand hidden-dark">
               <img src="images/mulainvest.png" width="70px">
             </a>
-           
+
           </div>
         </div>
-        
+
         {{-- Navbar Admin --}}
         <div class="navbar-right">
           <ul class="navbar-nav nav-tabs ms-auto">
@@ -34,7 +34,7 @@
                 aria-expanded="false">Admin</span>
               <ul class="dropdown-menu dropdown-menu-end">
                 <li>
-                  <a class="dropdown-item" href="{{ route('berandaTamu') }}">
+                  <a class="dropdown-item" href="{{ route('logout') }}">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 icon icon-tabler icon-tabler-logout"
                       width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
                       stroke-linecap="round" stroke-linejoin="round">
