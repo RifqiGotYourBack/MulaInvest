d92ff24d7722ce18b5e9b21df33854f51ea73fa9
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-11-10 22:03:10 +0700

    Added TransactionHistory Controller

---
+++ mulainvest/app/Http/Controllers/TransactionHistoryController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\TransactionHistories;
+
+class TransactionHistoryController extends Controller
+{
+    public function index()
+    {
+        $userId = auth()->user()->UserID;
+        $transactionHistories = TransactionHistories::where('UserID', $userId)
+                                                    ->orderBy('TransactionDate', 'desc')
+                                                    ->paginate(10);
+
+        return view('riwayatTransaksi', compact('transactionHistories'));
+    }
+}
