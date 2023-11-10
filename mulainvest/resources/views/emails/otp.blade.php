d03b499a4eae92bb90109a555129f07aced7c18f
sfmuazam <sfmuazam@gmail.com>
2023-11-10 12:32:45 +0700

    Add OTP Verification

--- mulainvest/resources/views/emails/otp.blade.php
+++ mulainvest/resources/views/emails/otp.blade.php
@@ -4,6 +4,14 @@
     <title>OTP Email</title>
 </head>
 <body>
-    <p>Your OTP is: {{ $otp }}</p>
+    <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 20px auto;">
+        <h2>One-Time Password (OTP)</h2>
+        <p>Your OTP for verification is:</p>
+        <h1 style="background-color: #f0f0f0; padding: 10px; border-radius: 5px;">{{ $otp }}</h1>
+        <p>Please use this OTP to verify your account.</p>
+        <p>If you did not request this OTP, please ignore this email.</p>
+        <hr>
+        <p style="color: #999;">This email was sent from our application for OTP verification.</p>
+    </div>
 </body>
 </html>
